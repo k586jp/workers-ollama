b35ef596b18e94a05c4ed613c235b0c3ab3a68fc
k586 <mail@k586.jp>
2026-09-11 23:55:34 +0900

    AI Gateway を設定

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import {Hono, Context as c, Next} from 'hono';
 type Env = {
     Bindings: {
         OLLAMA_VPC: Fetcher,
-        AI_GATEWAY: Fetcher,
         SECRET_KEY: string
     }
 };
@@ -17,12 +16,11 @@ export default app;
 
 function proxyHandler(context: c) {
     const url = new URL(context.req.url);
-    // const gatewayBaseUrl = 'https://ollama-gateway.k586.jp/custom/ollama-local';
-    const targetUrl = 'http://ai-gateway/custom/ollama-local' + url.pathname + url.search;
+    const gatewayBaseUrl = 'https://ollama-gateway.k586.jp/custom/ollama-local';
+    const targetUrl = gatewayBaseUrl + url.pathname + url.search;
 
     const headers = new Headers(context.req.raw.headers);
     headers.delete('host');
-    headers.delete('x-api-key');
     const requestInit: RequestInit = {
         method: context.req.method,
         headers: headers,
@@ -32,17 +30,13 @@ function proxyHandler(context: c) {
     };
 
     const proxyRequest = new Request(targetUrl, requestInit);
-    // const requestInitCfProperties: RequestInit = {
-    //     // @ts-ignore
-    //     fetch: context.env.OLLAMA_VPC.fetch.bind(context.env.OLLAMA_VPC)
-    // };
-    // const result = fetch(proxyRequest, requestInitCfProperties);
-    const json: RequestInit = {
+    const requestInitCfProperties: RequestInit = {
         // @ts-ignore
         fetch: context.env.OLLAMA_VPC.fetch.bind(context.env.OLLAMA_VPC)
-    }
+    };
+    const result = fetch(proxyRequest, requestInitCfProperties);
 
-    return context.env.AI_GATEWAY.fetch(proxyRequest, json);
+    return result;
 }
 
 // ================================================================
